1d9f7b618fde27d0a7d2d8ea355f010a35b23c41
boncabr <ariboncabe100@gmail.com>
2026-06-05 18:08:58 +0700

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -74,23 +74,7 @@ async function handlePlay(client, ctx, queryStr) {
     await play(player, tracks);
 
     // 🔊 Bass + Vocal Boost default
-    await player.filterManager.setEQ([
-      { band: 0,  gain: 0.25 },
-      { band: 1,  gain: 0.25 },
-      { band: 2,  gain: 0.20 },
-      { band: 3,  gain: 0.10 },
-      { band: 4,  gain: 0.05 },
-      { band: 5,  gain: 0.05 },
-      { band: 6,  gain: 0.10 },
-      { band: 7,  gain: 0.15 },  // Vokal boost
-      { band: 8,  gain: 0.15 },  // Vokal boost
-      { band: 9,  gain: 0.10 },
-      { band: 10, gain: 0.05 },
-      { band: 11, gain: 0.00 },
-      { band: 12, gain: -0.05 },
-      { band: 13, gain: -0.05 },
-      { band: 14, gain: -0.05 },
-    ]);
+    
 
     const isNowPlaying = !player.queue.previous && player.queue.tracks.length <= tracks.length;
     const embed = createEmbed({
