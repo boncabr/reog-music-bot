7c0494889f3bfb49b4af6e8573bf351662409f1c
boncabr <ariboncabe100@gmail.com>
2026-06-16 15:51:30 +0700

    feat: clear voice emoji on force-disconnect (artifacts)

--- artifacts/discord-bot/src/events/voiceStateUpdate.js
+++ artifacts/discord-bot/src/events/voiceStateUpdate.js
@@ -1,5 +1,5 @@
 const logger = require('../utils/logger');
-const { isRadioMode, setAutoplay } = require('../music/MusicManager');
+const { isRadioMode, setAutoplay, clearVoiceEmoji } = require('../music/MusicManager');
 
 module.exports = {
   name: 'voiceStateUpdate',
@@ -30,6 +30,8 @@ module.exports = {
         // Matikan autoplay saat bot keluar dari VC
         setAutoplay(guildId, false);
         logger.debug(`Autoplay dimatikan karena bot keluar dari VC di guild ${guildId}`);
+        // Reset emoji ke default saat bot keluar VC (force-disconnect)
+        clearVoiceEmoji(guildId);
 
         // Try reconnect up to 3 times with back-off
         let attempts = 0;
