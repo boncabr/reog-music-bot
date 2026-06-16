7b5455d1e296abfee88a5523978b765a4c6fc5c9
boncabr <ariboncabe100@gmail.com>
2026-06-16 15:44:22 +0700

    fix: reset autoplay to off on playerDestroy

--- src/handlers/lavalinkHandler.js
+++ src/handlers/lavalinkHandler.js
@@ -1,5 +1,5 @@
 const logger = require('../utils/logger');
-const { setVoiceStatus, cacheTrack, handleAutoplay, isRadioMode, getAutoplay, updateAutoplaySeed } = require('../music/MusicManager');
+const { setVoiceStatus, cacheTrack, handleAutoplay, isRadioMode, getAutoplay, setAutoplay, updateAutoplaySeed } = require('../music/MusicManager');
 
 const BOLD_MAP = {
   a:'𝗮',b:'𝗯',c:'𝗰',d:'𝗱',e:'𝗲',f:'𝗳',g:'𝗴',h:'𝗵',i:'𝗶',j:'𝗷',k:'𝗸',l:'𝗹',m:'𝗺',
@@ -269,6 +269,8 @@ async function loadLavalinkEvents(client) {
 
   client.lavalink.on('playerDestroy', (player, reason) => {
     logger.debug(`Player destroyed in guild ${player.guildId}: ${reason || 'unknown'}`);
+    // Matikan autoplay saat player di-destroy (bot keluar VC via ?leave, ?stop, atau VC kosong)
+    setAutoplay(player.guildId, false);
   });
 
   client.lavalink.on('playerCreate', (player) => {
